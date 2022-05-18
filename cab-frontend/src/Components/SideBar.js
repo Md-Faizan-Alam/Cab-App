7cb151baef133608f0c806ac8f4474addf236c21
Faizan <fajjualam@gmail.com>
2022-05-18 11:11:35 +0530

    FZN: rectified SideBar

--- cab-frontend/src/Components/SideBar.js
+++ cab-frontend/src/Components/SideBar.js
@@ -4,17 +4,17 @@ const SideBar = (props)=>{
     return(
         <>
          <div className="btn-group-vertical position-absolute start-0" style={{marginTop: '5vw'}} role="group" aria-label="Basic radio toggle button group">
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked/>
-            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} for="btnradio1">Profile</label>
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" defaultChecked/>
+            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} htmlFor="btnradio1">Profile</label>
           
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off"/>
-            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} for="btnradio2">Trips</label>
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off"/>
+            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} htmlFor="btnradio2">Trips</label>
           
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off"/>
-            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} for="btnradio3">Feedback</label>
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off"/>
+            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} htmlFor="btnradio3">Feedback</label>
 
-            <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autocomplete="off"/>
-            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} for="btnradio3">Delete Account</label>
+            <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autoComplete="off"/>
+            <label className="btn btn-outline-warning" style={{width: '20vw',fontSize: '2vw'}} htmlFor="btnradio3">Delete Account</label>
           </div>
         </>
     );
